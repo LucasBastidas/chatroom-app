7cd20865d2702420ddd12507b45d796d3eaf3475
Lucas Bastidas <lucaasbastidas@hotmail.com>
2022-06-09 11:00:02 -0300

    key.json update

--- database.ts
+++ database.ts
@@ -2,7 +2,7 @@ import * as admin from "firebase-admin";
 // import * as serviceAccount from "./key.json";
 
 admin.initializeApp({
-	credential: admin.credential.cert(process.env.KEYJSON),
+	credential: admin.credential.cert(process.env.GOOGLE_CONFIG_BASE64),
 	databaseURL: "https://apx-dwf-m6-72162-default-rtdb.firebaseio.com",
 });
 const firestore = admin.firestore();
